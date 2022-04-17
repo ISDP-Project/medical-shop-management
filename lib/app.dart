f4dc3eb6e24be12923c8dc998c031f49e6561da1
sahej-dev <sarao_sahej@outlook.com>
2022-04-17 12:53:30 +0530

    minor additions

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './constants/ui_decorations.dart';
+
 class App extends StatelessWidget {
-  const App({ Key? key }) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        iconTheme: const IconThemeData(
+          color: kDefaultLightPrimary,
+        ),
+      ),
+      home: SafeArea(
+        child: Container(),
       ),
     );
   }
---
+++ lib/constants/ui_strings.dart
@@ -0,0 +1,2 @@
+const String kAddStockPopupMessage =
+    "Are you sure you want to add selected medicines to the stock?";
