6aa530532654bc470e5c4ab700e211a17edb2d45
palashmunshi <palashmunshi4@gmail.com>
2022-04-25 18:08:08 +0530

    master db repository changes

--- packages/master_db_repository/lib/src/master_db_repository.dart
+++ packages/master_db_repository/lib/src/master_db_repository.dart
@@ -4,21 +4,21 @@ import 'dart:html';
 import 'package:supabase/supabase.dart';
 
 class MasterDBHandler {
-
   SupabaseClient _supabase;
 
   MasterDBHandler(this._supabase);
 
-  Future<List> readMedicine() async {
-    var response = await _supabase.from("Medicine").select().execute();
+  Future<List> getMedicine() async {
+    final PostgrestResponse response =
+        await _supabase.from("Medicine").select().execute();
     print(response);
     final dataList = response.data as List<dynamic>;
     return dataList;
   }
 
-  void addMedicine(int barcodeNumber, String medName, String medType, int medQuantity,
-      int medPrice, String medDescription) async {
-    var response = await _supabase.from("Medicine").insert({
+  void addMedicine(int barcodeNumber, String medName, String medType,
+      int medQuantity, int medPrice, String medDescription) async {
+    final PostgrestResponse response = await _supabase.from("Medicine").insert({
       'med_id': barcodeNumber,
       'med_name': medName,
       'med_type': medType,
@@ -30,7 +30,7 @@ class MasterDBHandler {
   }
 
   void updateMedicine(int barcodeNumber, int medQuantity, int medPrice) async {
-    var response = _supabase
+    final PostgrestResponse response = await _supabase
         .from("Medicine")
         .update({'med_Quantity': medQuantity, 'med_Price': medPrice})
         .eq('med_id', barcodeNumber)
