c4f722fa285c171424ddbc32118ab5edab2a4cac
parthareja <parth.thareja.pt@gmail.com>
2022-04-25 23:15:40 +0530

    all functions updated and tested except for +-multiple

--- packages/pharmacy_data_repository/lib/src/pharmacy_data_repository.dart
+++ packages/pharmacy_data_repository/lib/src/pharmacy_data_repository.dart
@@ -7,34 +7,27 @@ class PharmacyDataRepository {
   PharmacyDataRepository(this._supabaseClient, this.pharmacyTableName);
 
   void addQuantity({
-    // ON-PURCHASE // if item already exists in database, update quantity
-    required final String itemID, // itemID: int
+    required final String itemID,
     required final int quantity,
   }) async {
     if ((await checkItemExistence(itemID: itemID)) == false) {
       addNewItem(itemID: itemID, quantity: quantity);
-
     } else {
       final curTemp = await _supabaseClient
           .from(pharmacyTableName)
           .select('quantity')
           .eq('item_id', itemID)
-          .execute(); // pharmacyTableName = name of table of that pharmacy
+          .execute();
 
-      int currentItemQuantity = curTemp.data[0][
-          'quantity']; // #################### this might be wrong, refer to authentication repository
+      int currentItemQuantity = curTemp.data[0]['quantity'];
       final int finalQuantity = quantity + currentItemQuantity;
 
       await _supabaseClient
           .from(pharmacyTableName)
-          .update({
-            'quantity': finalQuantity
-          }) // if doesnt work, convert finalQuantity to String
+          .update({'quantity': finalQuantity})
           .eq('item_id', itemID)
           .execute();
     }
-    // if item already in table: ##########################################
-    // else: addNewItem({itemID, quantity}) ################################
   }
 
   void addMultipleQuantities({required final Map<int, int> items}) async {
@@ -48,14 +41,12 @@ class PharmacyDataRepository {
     required final String itemID,
     required final int quantity,
   }) async {
-    // if item not in inventory, call this fxn basically
     await _supabaseClient
         .from(pharmacyTableName)
         .insert({'item_id': itemID, 'quantity': quantity}).execute();
   }
 
   void removeQuantity({
-    // ON-SALE
     required final String itemID,
     required final int quantity,
   }) async {
@@ -65,23 +56,17 @@ class PharmacyDataRepository {
         .eq('item_id', itemID)
         .execute();
 
-    print(curTemp.data);
-
     int currentItemQuantity = curTemp.data[0]['quantity'];
-    final int finalQuantity =
-        currentItemQuantity - quantity; // finalQuantity datatype?
+    final int finalQuantity = currentItemQuantity - quantity;
 
     await _supabaseClient
         .from(pharmacyTableName)
-        .update({
-          'quantity': finalQuantity
-        }) // if doesnt work, convert finalQuantity to String
+        .update({'quantity': finalQuantity})
         .eq('item_id', itemID)
         .execute();
   }
 
   void removeMultipleQuantities({required final Map<int, int> items}) async {
-    // ON-SALE // if multiple items scanned together
     // write code here
   }
 
@@ -96,39 +81,8 @@ class PharmacyDataRepository {
     return true;
   }
 }
-
-
-
-// check item existence
-// multipleAdd
-// mutlipleRemove
-// testing sab functions ki wfjvweyrkwoid,;ekwnifvwehn
-
-
-/////////////////////////////
-
-// upsert
-// check if item already exists or not
-
-// take a list as well
-
-// run code
-
-// change barcode id to med_id and Quantity to quantity
-
-// DML commands likh, DDL will be done db side pe
-
-// run and test this code?
-// PLPGSQL some shit udyhewaxyru
-// if not returning enything, do 'return type = void'
+// addMultipleQuantities
+// removeMultipleQuantities
+// test all functions
+// remind sahej to make a trigger that creates a pharmacy table
 // remind costPrice to sahej during DB work
-
-// MAYBE IDK:
-
-//  at login,
-//           can update table via barcode
-
-//  at signup (new user)
-//           creates new table for pharmacy
-
-// palash table med id kya hota hau medicined.id implies id (palashtable colum naming conventions fix)
