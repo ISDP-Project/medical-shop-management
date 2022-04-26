1871a2be59100154509b22d4e5d38383424ce4da
vishishta04 <vishishtaiit@gmail.com>
2022-04-26 23:48:17 +0530

    half done bililng page

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
+import './billing_page/billing_page.dart';
+// import './billing_page/view/billing_page.dart';
+
 class App extends StatelessWidget {
-  const App({ Key? key }) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        
+    return MaterialApp(
+      home: SafeArea(
+        child: BillingPage(),
       ),
     );
   }
